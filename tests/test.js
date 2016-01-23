47a4d3e6df4a5d26897135a8962b7a16f196c36d
vrunoa <vruno.alassia@gmail.com>
2016-01-22 16:55:33 -0800

    update dependencies and test

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 import api from '../lib/index.js' 
+import server from '../lib/server.js'
 import isPlainObj from 'is-plain-obj'
+import request from 'request'
 
 describe("Test mibicitubici service api", () => {
   
@@ -19,6 +21,22 @@ describe("Test mibicitubici service api", () => {
       if(!data.estaciones)
         throw new Error("Error parsing result, "+data)
       done()
+    })
+  })
+
+  it("Test server response", (done) => {
+    let promise = api()
+    promise.then( (data) => {
+      let app = server(5000)
+      request.get("http://localhost:5000", (err, body, response) => {
+        if(err) throw new Error(err.message)
+        let estaciones = eval(response)
+        if(!estaciones) throw new Error("Error parsing data, "+response)
+        let strEstaciones = JSON.stringify(estaciones)
+        let strData = JSON.stringify(data.estaciones)
+        if(strEstaciones != strData) throw new Error("Error data response", +response)
+        done()
+      })
     })
   })
 })
